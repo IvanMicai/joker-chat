83d8a14359e4ea86f0eba7fede1eea75604a301f
Ivan Micai da Silva <ivanmicai@gmail.com>
2024-04-25 02:16:50 -0300

    feat: add language params (querystring)

--- app/api/joker/route.ts
+++ app/api/joker/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-const apiKey =process.env.SERVER_API_KEY
+const apiKey = process.env.SERVER_API_KEY
 
 type Joker = {
   error: boolean,
@@ -36,7 +36,17 @@ export async function POST(req: NextRequest, res: NextResponse) {
     return Response.json("Not authorized", { status: 401 })
   }
 
-  const response = await fetch("https://v2.jokeapi.dev/joke/Any?lang=pt")
+  const searchParams = req.nextUrl.searchParams
+  const lang = searchParams.get("lang") != null
+    ? `${searchParams.get("lang")}`
+    : 'en'
+
+  // Validate Language
+  if (!['pt', 'en'].includes(lang)) {
+    return Response.json("Language not supported. Must be ['en', 'pt']", { status: 422 })
+  }
+
+  const response = await fetch(`https://v2.jokeapi.dev/joke/Any?lang=${lang}`)
 
   if (response.status == 429) {
     // TODO: Log Joker Ratelimit Api
