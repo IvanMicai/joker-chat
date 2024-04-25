f584e5d3c624d4229b80097739d9d87099743db2
Ivan Micai da Silva <ivanmicai@gmail.com>
2024-04-25 15:12:13 -0300

    chore: adjust handler

--- app/api/joker/route.ts
+++ app/api/joker/route.ts
@@ -7,14 +7,14 @@ export async function POST(req: NextRequest, res: NextResponse) {
   // Get request data
   const signature = req.headers.get("x-moveo-signature")
   const rawBody = await req.text()
-  const { context: { user: { lang }}} = JSON.parse(rawBody)
-
+  
   // Check Webhook signature
   if (!validateSignature(rawBody, signature)) {
     return Response.json("Not authorized", { status: 401 })
   }
 
   // Get Joke
+  const { context: { user: { lang }}} = JSON.parse(rawBody)
   const joke = await getJoke(lang)
   
   return Response.json({
