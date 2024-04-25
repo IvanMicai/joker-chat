5c935c9748f9ddac98f7abdb2eeaba87fcab2f48
Ivan Micai da Silva <ivanmicai@gmail.com>
2024-04-25 01:44:21 -0300

    feat: add server apiKey

--- app/api/joker/route.ts
+++ app/api/joker/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
+const apiKey =process.env.SERVER_API_KEY
 
 type Joker = {
   error: boolean,
@@ -31,6 +32,10 @@ const jokerTired = {
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
+  if (apiKey !== req.headers.get("x-api-key")) {
+    return Response.json("Not authorized", { status: 401 })
+  }
+
   const response = await fetch("https://v2.jokeapi.dev/joke/Any?lang=pt")
 
   if (response.status == 429) {
